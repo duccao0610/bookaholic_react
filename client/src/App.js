47510c9b0944770db719a66aa864fece24a344f9
duccao0610 <caominhduc610@gmail.com>
2021-07-08 09:00:11 +0700

    Book trending

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
-
+import { Navbar, Form, FormControl } from "react-bootstrap";
+import { Route } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
-  return <div className="App">Hello I'm Client</div>;
+  return (
+    <div className="App">
+      <Navbar
+        bg="primary"
+        variant="dark"
+        className="d-flex justify-content-between py-4"
+      >
+        <Navbar.Brand href="#home">Bookaholic</Navbar.Brand>
+        <Form inline>
+          <FormControl
+            type="text"
+            placeholder="Search here..."
+            className="mr-sm-2"
+          />
+        </Form>
+      </Navbar>
+      <Route path="/" exact component={Home} />
+    </div>
+  );
 }
 
 export default App;
