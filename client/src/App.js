aae8221d61eb29f18c795b8cfd6f6b56667069d5
Hung Nguyen <hungngk98@gmail.com>
2021-07-08 12:21:00 +0700

    fix home layout  & activity component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import "./App.css";
-import { Navbar, Form, FormControl } from "react-bootstrap";
-import { Route } from "react-router-dom";
-import Home from "./pages/Home";
+import './App.css';
+import { Navbar, Form, FormControl } from 'react-bootstrap';
+import { Route } from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar
-        bg="primary"
-        variant="dark"
-        className="d-flex justify-content-between py-4"
-      >
-        <Navbar.Brand href="#home">Bookaholic</Navbar.Brand>
+        bg='primary'
+        variant='dark'
+        className='d-flex justify-content-between py-4 px-3'>
+        <Navbar.Brand className='brand' href='#home'>
+          Bookaholic
+        </Navbar.Brand>
         <Form inline>
           <FormControl
-            type="text"
-            placeholder="Search here..."
-            className="mr-sm-2"
+            type='text'
+            placeholder='Search here...'
+            className='mr-sm-2'
           />
         </Form>
       </Navbar>
-      <Route path="/" exact component={Home} />
+      <div className='content'>
+        <Route path='/' exact component={Home} />
+      </div>
     </div>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,12 @@
-import { Tabs, Tab, Spinner, Container, Row, Col } from "react-bootstrap";
-import { useState, useEffect } from "react";
-import BookOnTrend from "../components/BookOnTrend";
+import { Tabs, Tab, Spinner, Container, Row, Col } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import BookOnTrend from '../components/BookOnTrend';
+import Activity from '../components/Activity';
 const Home = () => {
   const [booksData, setBooksData] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    fetch("http://192.168.0.102:5000/books")
+    fetch('http://localhost:5000/books')
       .then((res) => res.json())
       .then((resJson) => {
         console.log(resJson);
@@ -15,25 +16,42 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="w-100 vh-100">
-      <div className="home_tabs w-100 d-flex flex-column align-items-center py-3">
+    <div className='w-100 vh-100 d-flex'>
+      <div className=' bg-warning d-none d-md-inline-block d-lg-inline-block    vh-100 col-md-2 col-lg-2'>
+        Left
+      </div>
+      <div className='home_tabs w-100 d-flex flex-column align-items-center py-3 col-12  col-md-8 col-lg-8 '>
         <Tabs
-          variant="pills"
-          defaultActiveKey="trending"
-          id="uncontrolled-tab-example"
-        >
-          <Tab eventKey="feeds" title="Feeds">
-            <div className="tab_feeds_content">Feeds</div>
+          variant='pills'
+          defaultActiveKey='trending'
+          id='uncontrolled-tab-example'>
+          <Tab eventKey='feeds' title='Feeds'>
+            <div className='tab_feeds_content mt-3 p-5  border border-dark w-100 '>
+              <Activity
+                username='username'
+                bookName='Matbiec'
+                date='2021'
+                rating={4}
+                review='A very good book'
+                cover='https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1546928634l/43505141.jpg'
+              />
+            </div>
           </Tab>
-          <Tab eventKey="trending" title="Trending">
-            <div className="tab_trending_content mt-3 text-center">
+          <Tab eventKey='trending' title='Trending'>
+            <div className='tab_trending_content mt-3 text-center'>
               {loading ? (
-                <Spinner animation="border" variant="primary" />
+                <Spinner animation='border' variant='primary' />
               ) : (
                 <Container>
                   <Row>
                     {booksData.map((book) => (
-                      <Col key={book._id} xs={12} sm={12} md={6} lg={6} xl={4}>
+                      <Col
+                        key={book._id}
+                        xs={12}
+                        sm={12}
+                        md={12}
+                        lg={12}
+                        xl={6}>
                         <BookOnTrend book={book}></BookOnTrend>
                       </Col>
                     ))}
@@ -43,6 +61,9 @@ const Home = () => {
             </div>
           </Tab>
         </Tabs>
+      </div>
+      <div className='bg-danger d-none d-md-inline-block d-lg-inline-block col-md-2 col-lg-2'>
+        Right
       </div>
     </div>
   );
