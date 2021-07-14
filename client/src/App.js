c54a5c492c6b17a5df3054f330bd122bc6dfef80
duccao0610 <caominhduc610@gmail.com>
2021-07-14 21:13:18 +0700

    Use NavBar a seperate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
-import { Navbar, Form, FormControl } from 'react-bootstrap';
-import { Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
+import "./App.css";
+import { Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 import NavBar from "./components/NavBar";
 import BookDetail from "./components/BookDetail";
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar/>
-      <div className='content'>
-        <Route path='/profile' exact component={Profile} />
+    <div className="App">
+      <NavBar />
+      <div className="content">
+        <Route path="/profile" exact component={Profile} />
         <Route path="/books/:id" component={BookDetail} />
-        <Route path='/' exact component={Home} />
+        <Route path="/" exact component={Home} />
       </div>
     </div>
   );
