9fbf1224bc735d4e1eab242c3cd7e3576783821e
duccao0610 <caominhduc610@gmail.com>
2021-07-21 23:37:49 +0700

    imported  ScrollToTop

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,10 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import ScrollToTop from "./components/ScrollToTop";
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
