231db5479104f9b9d184a569c35bc71f15c02375
duccao0610 <caominhduc610@gmail.com>
2021-07-21 23:41:35 +0700

    handle search form

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,26 +1,64 @@
 import { Navbar, Form, FormControl } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
+import Logo from "../Logo.png";
+import { useState } from "react";
+import "./NavBar.css";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import SearchResultsOverlay from "./SearchResultsOverlay";
 const NavBar = () => {
+  const { pathname } = useLocation();
+  const [resultsVisibility, setResultsVisibility] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const reg = /\s\s+/g;
+  const clearedSearchValue = searchValue.replace(reg, " ").trim();
+  console.log("searc", "[" + searchValue + "]");
+  console.log("clear", "[" + clearedSearchValue + "]");
+  const handleSearch = (e) => {
+    setResultsVisibility(true);
+    setSearchValue(e.target.value);
+  };
+  const handleBlur = () => {
+    setResultsVisibility(false);
+    setSearchValue("");
+  };
+
+  useEffect(() => {
+    handleBlur();
+  }, [pathname]);
+
+  useEffect(() => {
+    if (searchValue === "") {
+      handleBlur();
+    }
+  }, [searchValue]);
+
   return (
-    // <div>
     <Navbar
       bg="primary"
       variant="dark"
-      className="d-flex justify-content-between py-4 px-3"
+      className="py-2 row fixed-top w-100 m-0"
     >
-      <Link to="/" className="text-decoration-none">
-        <div className="text-dark">Bookaholic</div>
+      <Link to="/" className="text-decoration-none col-4 col-lg-2 mx-lg-4 pl-0">
+        <img style={{ width: "100px" }} alt="" src={Logo} />
       </Link>
-      <Form inline>
-        <FormControl
-          type="text"
-          placeholder="Search here..."
-          className="mr-sm-2"
-        />
+      <Form inline className=" col-8 col-lg-3 p-0">
+        <div className="form_control w-100">
+          <FormControl
+            type="text"
+            placeholder="Search here..."
+            className="w-100 rounded-0 border-0"
+            value={searchValue}
+            onChange={handleSearch}
+          />
+          {resultsVisibility ? (
+            <div className="search_overlay_container">
+              <SearchResultsOverlay searchValue={clearedSearchValue} />
+            </div>
+          ) : null}
+        </div>
       </Form>
     </Navbar>
-    // </div>
   );
 };
 
